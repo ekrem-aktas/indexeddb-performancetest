7c359e50652439dd7d74d878767907a793470d84
Ekrem Aktas <ekrem.aktas@mendix.com>
2020-03-25 15:51:09 +0100

    Add new test case

--- src/testcases.js
+++ src/testcases.js
@@ -43,6 +43,33 @@ export function * getTestCases() {
     yield insert(tableWith2Columns, generateObjects(20000, generateObjWith2Columns));
     yield query(tableWith2Columns, [["col1", VALUES[0]]]);
     yield cleanTable(tableWith2Columns);
+
+    // 5 columns
+    const tableWith5Columns = "Table5Cols";
+    const generateObjWith5Columns = i => ({
+        col1: VALUES[i % VALUES.length],
+        col2: i.toString(),
+        col3: "Test Test " + i.toString(),
+        col4: i % 2 === true,
+        col5: new Date().getTime() + i
+    });
+    yield createTable(tableWith5Columns, "col1", "col2", "col3", "col4", "col5")
+    // 100x5cols
+    yield insert(tableWith5Columns, generateObjects(100, generateObjWith5Columns));
+    yield query(tableWith5Columns, [["col1", VALUES[0]]]);
+    yield cleanTable(tableWith5Columns);
+    // 1Kx5cols
+    yield insert(tableWith5Columns, generateObjects(1000, generateObjWith5Columns));
+    yield query(tableWith5Columns, [["col1", VALUES[0]]]);
+    yield cleanTable(tableWith5Columns);
+    // 10Kx5cols
+    yield insert(tableWith5Columns, generateObjects(10000, generateObjWith5Columns));
+    yield query(tableWith5Columns, [["col1", VALUES[0]]]);
+    yield cleanTable(tableWith5Columns);
+    // 20Kx5cols
+    yield insert(tableWith5Columns, generateObjects(20000, generateObjWith5Columns));
+    yield query(tableWith5Columns, [["col1", VALUES[0]]]);
+    yield cleanTable(tableWith5Columns);
 }
 
 function createTable(name, ...columns) {
