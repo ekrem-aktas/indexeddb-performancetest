f6223c61fedf90a394834baa9a56dd57a4675d64
Ekrem Aktas <ekrem.aktas@mendix.com>
2022-12-15 15:35:57 +0300

    Use a single transaction for SQLite tests

--- src/sqlite.js
+++ src/sqlite.js
@@ -9,20 +9,13 @@ export async function initSqliteWasm() {
         sqlite3InitModule(self.sqlite3TestModule).then((sqlite3)=>{
             const oo = sqlite3.oo1;
             const capi = sqlite3.capi;
-            if (capi.sqlite3_vfs_find(null) === 0) {
-                reject("Ooops");
-            }
-
             if(!capi.sqlite3_vfs_find('kvvfs')){
               reject("This build is not kvvfs-capable.");
             }
 
-            console.warn(`Origin-Private FileSystem supported: ${!!sqlite3.oo1.OpfsDb}`)
-
-            const dbStorage = 0 ? 'session' : 'local';
-            db = new oo.JsStorageDb( dbStorage );
-            
-            console.log("db.storageSize():",db.storageSize());
+            console.warn(`Origin-Private FileSystem supported: ${!!sqlite3.oo1.OpfsDb}`);
+            db = new oo.JsStorageDb('local');
+            db.clearStorage();
 
             resolve();
         }).catch(reject);
@@ -43,11 +36,16 @@ export function exec(sql, params) {
     return result;
 }
 
-function execMultiple(sqls) {
-    const results = new Array(sqls.length);
-    for(let i = 0; i < sqls.length; i++) {
-        results[i] = exec(sqls[i][0], sqls[i][1]);
-    }
+function execMultiple(sqls, results) {
+    db.transaction(tx => {
+        for(let i = 0; i < sqls.length; i++) {  
+            results[i] = tx.exec({
+                sql: sqls[i][0],
+                bind: sqls[i][1],
+                returnValue: "resultRows"
+            });
+        }
+    });
     return results;
 }
 
@@ -62,7 +60,7 @@ export function runTestWithSQLite(testCase) {
         }
         case "insert": {
             const stats = generateInsertStatements(testCase.table, testCase.data);
-            return executeMultiple(stats);
+            return executeMultiple(stats, new Array(stats.length));
         }
         case "query": {
             const [sql, p] = generateSelectStatement(testCase.table, testCase.filter);
